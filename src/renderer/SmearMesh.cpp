4f2f49e58c8a24391bce3fb82d8e022e571c4989
Francisco Henriques <franciscolealhenriques@usp.br>
2026-06-19 07:56:04 +0200

    Add final calculation of deltas in SmearMesh

--- src/renderer/SmearMesh.cpp
+++ src/renderer/SmearMesh.cpp
@@ -1,88 +1,302 @@
+#include <string>
+#include <unistd.h>
 #include "SmearMesh.h"
 #include "assimp/anim.h"
+#include "glm/ext/quaternion_geometric.hpp"
+#include "glm/ext/vector_float3.hpp"
+#include "glm/gtx/string_cast.hpp"
+#include "renderer/AnimatedMesh.h"
 
 
 SmearMesh::SmearMesh(const std::string &path) : AnimatedMesh(path) {
+  computeVertexSets();
   generateDeltasSSBO();
+
+  // DEBUG
+  glCreateVertexArrays(1, &m_debugVao);
+  glCreateBuffers(1, &m_debugVbo);
+  glVertexArrayVertexBuffer(m_debugVao, 0, m_debugVbo, 0, sizeof(glm::vec3));
+  glEnableVertexArrayAttrib(m_debugVao, 0);
+  glVertexArrayAttribFormat(m_debugVao, 0, 3, GL_FLOAT, GL_FALSE, 0);
+  glVertexArrayAttribBinding(m_debugVao, 0, 0);
+}
+
+// builds V_kr and V_kt
+void SmearMesh::computeVertexSets() {
+  m_V_kr.resize(m_bonesInfo.size());
+  m_V_kt.resize(m_bonesInfo.size());
+  for (auto const& [boneName, boneIdx] : m_boneNameToIndexMap) { // that's cool I didn't know it was possible
+    const aiNode *currentNode = m_nodeMap[boneName];
+    // tip:
+    // 1. bone itself
+    // 2. all of its immediate children
+    std::vector<int> tipBones = { boneIdx };
+    std::vector<const aiNode*> childrenBones;
+    getChildrenBones(currentNode, childrenBones);
+    for (auto child : childrenBones) {
+      tipBones.push_back(m_boneNameToIndexMap[child->mName.C_Str()]);
+    }
+    // root:
+    // 1. bone itself
+    // 2. parent
+    // 3. siblings
+    std::vector<int> rootBones;
+    const aiNode* parentBone = getParentBone(currentNode);
+    if (parentBone != nullptr) {
+      rootBones.push_back(m_boneNameToIndexMap[parentBone->mName.C_Str()]);
+      std::vector<const aiNode *> siblingBones;
+      getChildrenBones(parentBone, siblingBones);
+      for (auto sibling : siblingBones) {
+        rootBones.push_back(m_boneNameToIndexMap[sibling->mName.C_Str()]);
+      }
+    } else {
+      rootBones.push_back(boneIdx);
+    }
+    auto getBonesVertices =
+      [&](const std::vector<int> &bones) -> std::vector<int> {
+    std::vector<int> vertices;
+    for (int i = 0; i < m_vertices.size(); ++i) {
+      for (int j = 0; j < MAX_NUM_BONES_PER_VERTEX; ++j) {
+
+        if (std::find(bones.begin(), bones.end(), m_vertices[i].bonesIDs[j]) !=
+            bones.end() && m_vertices[i].weights[j] > .0f) {
+          vertices.push_back(i);
+          break;
+        }
+      }
+    }
+    return vertices;
+  };
+    m_V_kt[boneIdx] = getBonesVertices(tipBones);
+    m_V_kr[boneIdx] = getBonesVertices(rootBones);
+  }
 }
 
 void SmearMesh::generateDeltasSSBO() {
-  // TODO for each bone, calculate all the deltas throughout the animation
-  // what data structure to use?
-  std::vector<float> frameDeltas;
-  std::vector<std::vector<float>> animationDeltas;
   // PSEUDOCODE
   // for (frame in frames) {
   //   calculateFrameDeltas(frame, frameDeltas);
   //   animationDeltas[i] = frameDeltas;
 // }
 // createAnimationDeltasSSBO(animationDeltas);
+  double ticksPerSecond = m_scene->mAnimations[0]->mTicksPerSecond != 0 ? m_scene->mAnimations[0]->mTicksPerSecond : 25.0f;
+  double tickStep = ticksPerSecond / m_Fps;
+  double totalDuration = m_scene->mAnimations[0]->mDuration;
+
+  std::vector<glm::mat4> currentTransforms(m_bonesInfo.size());
+  std::vector<glm::mat4> nextTransforms(m_bonesInfo.size());
+  std::vector<std::vector<float>> animationDeltas;
+  std::vector<glm::mat4> flatAnimationBonesTransforms;
+
+  for (double currentTick = 0; currentTick < totalDuration; currentTick += tickStep) {
+    double nextTick = currentTick + tickStep;
+    if (nextTick > totalDuration) {
+      nextTick = fmod(nextTick, totalDuration); // loop over
+    }
+    calculateGlobalTransformsAtTick(currentTick, m_scene->mRootNode, glm::mat4(1.0f), currentTransforms);
+    calculateGlobalTransformsAtTick(nextTick, m_scene->mRootNode, glm::mat4(1.0f), nextTransforms);
+
+    std::vector<glm::mat4> frameSkinnedBones = currentTransforms;
+    for (int i = 0; i < m_bonesInfo.size(); ++i) {
+      frameSkinnedBones[i] *= m_bonesInfo[i].offsetMatrix;
+    }
+    flatAnimationBonesTransforms.insert(flatAnimationBonesTransforms.end(), frameSkinnedBones.begin(), frameSkinnedBones.end());
+
+    std::vector<float> frameDeltas;
+    getFrameDeltas(currentTransforms, nextTransforms, frameSkinnedBones, frameDeltas);
+    animationDeltas.push_back(frameDeltas);
+    Log::info("Deltas generated for frame at tick " + std::to_string(currentTick));
+  }
+  m_totalFrames = animationDeltas.size();
+  std::vector<float> flatDeltas;
+  flatDeltas.reserve(
+      m_totalFrames *
+      m_vertices
+          .size()); // reserve() makes element insertion easier with insert()
+  for (const auto &frameDeltas : animationDeltas) {
+    flatDeltas.insert(flatDeltas.end(), frameDeltas.begin(), frameDeltas.end());
+  }
+  glCreateBuffers(1, &m_deltas_ssbo);
+  glNamedBufferData(m_deltas_ssbo, flatDeltas.size() * sizeof(float),
+                    flatDeltas.data(), GL_STATIC_DRAW);
+
+  // NOTE maybe either change the function's name or do that somewhere else
+  // because it can be confusing later to find where I create the transforms
+  // SSBO
+  glCreateBuffers(1, &m_animation_bones_transforms_ssbo);
+  glNamedBufferData(m_animation_bones_transforms_ssbo,
+                    flatAnimationBonesTransforms.size() * sizeof(glm::mat4),
+                    flatAnimationBonesTransforms.data(), GL_STATIC_DRAW);
 }
 
-// TODO add another uniform for the deltas and all the data needed for the smear frames
+// TODO add another uniform (SSBO) for the deltas and all the data needed for the smear frames;
 void SmearMesh::uploadUniforms(const Shader& shader, const RenderContext& ctx) const {
-    Mesh::uploadUniforms(shader,ctx);
-    std::vector<glm::mat4> transforms;
-    getBoneTransforms(transforms);
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, bones_data_ssbo);
-    for (int i = 0 ; i < m_bonesInfo.size(); ++i) {
-        shader.set(("gBones["+std::to_string(i)+"]").c_str(), transforms[i]);
+    AnimatedMesh::uploadUniforms(shader,ctx);
+
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, m_deltas_ssbo);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 4, m_animation_bones_transforms_ssbo);
+
+    double time = timer.getCurrentValue();
+    double ticks_per_second = m_scene->mAnimations[0]->mTicksPerSecond != 0 ? m_scene->mAnimations[0]->mTicksPerSecond : 25.0f;
+    double currentTick = fmod(time * ticks_per_second, m_scene->mAnimations[0]->mDuration);
+    double tickStep = ticks_per_second / m_Fps;
+    int currentFrame = static_cast<int>(currentTick / tickStep) % m_totalFrames;
+
+    shader.set("currentFrame", currentFrame);
+    shader.set("totalVertices", static_cast<int>(m_vertices.size()));
+    shader.set("totalFrames", m_totalFrames);
+    shader.set("betaMax", 1.0f);
+    shader.set("boneStride", static_cast<int>(m_bonesInfo.size())); // in the shader the max number of bones is hardcoded
+
+    std::vector<glm::mat4> currentTransforms;
+    getBoneTransforms(currentTransforms);
+
+    std::string boneName = "mixamorig:RightForeArm";
+    auto it = m_nodeMap.find("mixamorig:RightForeArm");
+    if (it != m_nodeMap.end()) {
+      const aiNode* debugNode = it->second;
+      glm::vec3 c_r, c_t;
+      const_cast<SmearMesh*>(this)->getBonePosition(debugNode, currentTransforms, c_r, c_t);
+      shader.set("debug_c_r", c_r);
+      shader.set("debug_c_t", c_t);
     }
 }
 
-void SmearMesh::getFrameDeltas(std::vector<float> &outFrameDeltas) {
-  std::vector<glm::mat4> boneTransforms; // global transforms
-  getBoneTransforms(boneTransforms);
+// calculate the deltas for a single frame
+void SmearMesh::getFrameDeltas(std::vector<glm::mat4> currentTransforms,
+                               std::vector<glm::mat4> nextTransforms,
+                               std::vector<glm::mat4> frameSkinnedBones,
+                               std::vector<float> &outFrameDeltas
+) {
+  // NOTE
+  // bone variables:
+  // - v_t, v_r, c_r, c_t, b, omega
+  // vertex variables:
+  // - u_i, v, n
   outFrameDeltas.assign(m_vertices.size(), 0);
+  // NOTE structure it in a way that when I access u_k, I have a list
+  //      of its values for all the bones that affect it. same for d_ik
+  std::vector<std::vector<float>> us; // u_ik
+  std::vector<std::vector<float>> deltas; // d_ik
 
-  // iterate through dict entries (pair<key, value>)
-  for (std::map<std::string, int>::iterator it = m_boneNameToIndexMap.begin(); it != m_boneNameToIndexMap.end(); ++it) {
+  // for all bones (nodes in the tree)
+  for (auto const& [boneName, boneIdx] : m_boneNameToIndexMap) {
     glm::vec3 ct, cr, next_ct, next_cr;
-    std::string boneName = it->first;
-    int boneIdx = it->second;
-    float boneLength = 1.0f; // TODO vai dar pra ser calculado com c_t e c_r
+    const aiNode* currentNode = m_nodeMap[boneName];
 
-    // how to extract all the transforms from the whole animation?
-    // probably go to each animation node (which represents the animation of a
-    // single bone throughout the whole animation) and compute the deltas for a
-    // single bone.
-    // So here I just have to, for each bone, get its vector of transformations
-    // in its respective animation node (TODO how to get the animation node
-    // associated with it??) and then calculate the deltas based on that
-    extractBonePosition(currentBoneTransforms[boneIdx], boneLength, cr, ct);
-    extractBonePosition(nextBoneTransforms[boneIdx], boneLength, next_cr, next_ct);
+    // get current and next bone positions
+    getBonePosition(currentNode, currentTransforms, cr, ct);
+    getBonePosition(currentNode, nextTransforms, next_cr, next_ct);
 
-    // and then, how to calculate the final delta for each vertex (that may
-    // potentially be affected by many bones)?
-    // TODO understand how the fuck they do that in the paper and implement it.
-    glm::vec3 vt_hat = glm::normalize(next_ct - ct);
-    glm::vec3 vr_hat = glm::normalize(next_cr - cr);
-    glm::vec3 b = ct - cr;
-    glm::vec3 b_hat = glm::normalize(b);
-    float omega = glm::acos(glm::dot(vt_hat, vr_hat));
-    auto S = [](const float x) -> float { return glm::smoothstep(0.0f, 1.0f, x); };
-    for (int i = 0; i < vertices.size(); ++i) {
-      const AnimatedVertex &vertex = vertices[i];
+    // calculate bone-dependent variables
+    glm::vec3 vr = next_cr - cr;
+    glm::vec3 vt = next_ct - ct;
+    float vtLen = glm::length(vt);
+    float vrLen = glm::length(vr);
+    if (vrLen < 1e-5f && vtLen < 1e-5f) continue;
+    // TODO maybe not the best fallback
+    // consider that tip is moving like the root or vice-versa as fallback
+    glm::vec3 vr_norm = vrLen > 1e-5f ? vr / vrLen : glm::vec3(0.0001);
+    glm::vec3 vt_norm = vtLen > 1e-5f ? vt / vtLen : glm::vec3(0.0001);
+    glm::vec3 b = ct - cr; // TEST inverse because I'm inverting it in the getBonePosition
+    if (glm::length(b) < 1e-5f) continue;
+    glm::vec3 b_norm = glm::normalize(b);
+    float omega = glm::acos(glm::clamp(glm::dot(vt_norm, vr_norm), -1.0f, 1.0f));
+    auto S = [](const float x) -> float {
+      float y = glm::clamp(x, 0.0f, 1.0f);
+      return 3 * pow(y, 2) - 2 * pow(y, 3);
+    };
+
+    auto calculateDelta_ik = [&](int vertexIdx) -> float {
+      const AnimatedVertex& v = m_vertices[vertexIdx];
+      // NOTE not sure if I have to put it in skinning (bone) space
+      glm::mat4 skinMatrix = glm::mat4(0.0f);
+      for (int j = 0; j < MAX_NUM_BONES_PER_VERTEX; ++j) {
+        if (v.weights[j] > 0.0f) {
+          skinMatrix += currentTransforms[v.bonesIDs[j]] * m_bonesInfo[v.bonesIDs[j]].offsetMatrix * v.weights[j];
+        }
+      }
+      glm::vec3 p_i = glm::vec3(skinMatrix * glm::vec4(v.position, 1.0f));
+      // glm::vec3 p_i = m_vertices[vertexIdx].position;
+      float u =
+        S(glm::dot(p_i - cr, b_norm) / glm::length(b));
+      glm::vec3 v_norm;
+      if (omega < 0.1f) {
+        v_norm = glm::normalize(glm::mix(vr_norm, vt_norm, u));
+
+      } else {
+        v_norm = glm::sin((1 - u) * omega) / glm::sin(omega) * vr_norm
+          + glm::sin(u * omega) / glm::sin(omega) * vt_norm;
+      }
+      glm::vec3 n = v_norm - (glm::dot(v_norm, b_norm)) * b_norm;
+      return glm::dot(p_i - cr, glm::normalize(n));
+    };
+
+    // calculate max delta for normalization
+    float maxRootDelta = 0.0001f;
+    for (int vertexIdx : m_V_kr[boneIdx]) {
+      float delta = calculateDelta_ik(vertexIdx);
+      if (glm::abs(delta) > maxRootDelta) maxRootDelta = delta;
+    }
+    float maxTipDelta = 0.0001f;
+    for (int vertexIdx : m_V_kt[boneIdx]) {
+      float delta = calculateDelta_ik(vertexIdx);
+      if (glm::abs(delta) > maxTipDelta) maxTipDelta = delta;
+    }
+
+    for (int i = 0; i < m_vertices.size(); ++i) {
+      // get the vertices that are affected by this bone
       float weight = 0.0f;
-      bool isAttached = false;
-      for (int j = 0; j < MAX_NUM_BONES_PER_VERTEX; j++) {
-        if (vertex.bonesIDs[j] == boneIdx && vertex.weights[j] > 0.0f) {
-          weight = vertex.weights[j];
-          isAttached = true;
+      for (int j = 0; j < MAX_NUM_BONES_PER_VERTEX; ++j) {
+        if (m_vertices[i].bonesIDs[j] == boneIdx && m_vertices[i].weights[j] > 0.0f) {
+          weight = m_vertices[i].weights[j];
           break;
         }
       }
-      if (!isAttached) continue;
+      if (weight == 0) continue;
 
-      float u_i =
-        S(glm::dot(vertex.position - cr, b_hat) / boneLength);
-      glm::vec3 v_hat = sin((1 - u_i) * omega) / sin(omega) * vr_hat + sin(u_i * omega) / sin(omega) * vt_hat;
-      glm::vec3 n = v_hat - (glm::dot(v_hat, b_hat)) * b_hat;
-      glm::vec3 n_hat = glm::normalize(n);
-      float w_coll = 1 - pow(glm::dot(v_hat, b_hat), 2);
-      float delta_i = glm::dot(vertex.position - cr, n_hat);
-      // TODO account for the influence of multiple bones on this vertex
-      // float delta_i_bar = w_coll * delta_i /
+      const AnimatedVertex& vertex = m_vertices[i];
+      // BUG is it that I am not taking the cr of the correct bone????
+      float u_ik = S(glm::dot(vertex.position - cr, b_norm) / glm::length(b));
+      glm::vec3 v_norm;
+      if (omega < 0.1f) {
+          v_norm = glm::normalize(glm::mix(vr_norm, vt_norm, u_ik));
+      } else {
+          v_norm = glm::sin((1.0f - u_ik) * omega) / glm::sin(omega) * vr_norm
+                 + glm::sin(u_ik * omega) / glm::sin(omega) * vt_norm;
+      }
+      glm::vec3 n = v_norm - (glm::dot(v_norm, b_norm)) * b_norm;
+      glm::vec3 n_norm = glm::normalize(n);
+      float w_coll = 1.0f - pow(glm::dot(v_norm, b_norm), 2);
+      float delta_ik = glm::dot(vertex.position - cr, n_norm);
+      float M_ik = (u_ik * glm::abs(maxTipDelta)) + ((1.0f - u_ik) * glm::abs(maxRootDelta));
+      float delta_ik_norm = w_coll * (delta_ik / M_ik);
+      outFrameDeltas[i] += weight * delta_ik_norm;
     }
   }
+}
+
+void SmearMesh::drawDebugBones(const Shader& shader, const RenderContext& ctx) const {
+    std::vector<glm::mat4> currentTransforms;
+    getBoneTransforms(currentTransforms);
+
+    std::vector<glm::vec3> lineVertices;
+    lineVertices.reserve(m_boneNameToIndexMap.size() * 2);
+
+    for (const auto& pair : m_boneNameToIndexMap) {
+        const aiNode* node = m_nodeMap.at(pair.first);
+        glm::vec3 cr, ct;
+
+        const_cast<SmearMesh*>(this)->getBonePosition(node, currentTransforms, cr, ct);
+
+        lineVertices.push_back(cr);
+        lineVertices.push_back(ct);
+    }
+
+    // Upload to GPU and draw
+    glNamedBufferData(m_debugVbo, lineVertices.size() * sizeof(glm::vec3), lineVertices.data(), GL_DYNAMIC_DRAW);
+
+    glBindVertexArray(m_debugVao);
+    glDrawArrays(GL_LINES, 0, static_cast<GLsizei>(lineVertices.size()));
+    glBindVertexArray(0);
 }
--- src/renderer/SmearMesh.h
+++ src/renderer/SmearMesh.h
@@ -5,6 +5,7 @@
 #include "renderer/Types_renderer.h"
 #include <vector>
 #pragma once
+#include <iostream>
 #include <map>
 #include <string>
 #include <cassert>
@@ -12,9 +13,9 @@
 #include <glm/glm.hpp>
 #include <glad/glad.h>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
 
-#include "RenderPipeline.h"
-#include "renderer/Mesh.h"
+#include "Mesh.h"
 #include "AnimatedMesh.h"
 
 class SmearMesh : public AnimatedMesh {
@@ -25,18 +26,30 @@ class SmearMesh : public AnimatedMesh {
     SmearMesh& operator=(const SmearMesh&) = delete;
     void uploadUniforms(const Shader &shader,
                         const RenderContext &ctx) const override;
+    void drawDebugBones(const Shader& shader, const RenderContext& ctx) const;
 
   private:
-    GLuint deltas_ssbo = 0;
+    GLuint m_deltas_ssbo = 0;
+    GLuint m_animation_bones_transforms_ssbo =
+        0; // 2D matrix that holds all bone transforms throughout the whole
+           // animation
+    // DEBUG
+    GLuint m_debugVao = 0;
+    GLuint m_debugVbo = 0;
+
+    int m_Fps = 30; // TODO get this information from main.cpp
+    int m_totalFrames;
+    std::vector<std::vector<int>> m_V_kr;
+    std::vector<std::vector<int>> m_V_kt;
 
     const std::map<PassTag, ShaderKey> shaderKeysMap() const override {
       return std::map<PassTag, ShaderKey> {
       {PassTag::Hybrid, ShaderKey{"hybrid_smear.vert", "hybrid.frag"}}};
     }
+    void computeVertexSets();
     void generateDeltasSSBO();
-    void getFrameDeltas(std::vector<float> &outFrameDeltas);
+    void getFrameDeltas(std::vector<glm::mat4> currentTransforms, std::vector<glm::mat4> nextTransforms, std::vector<glm::mat4> frameSkinnedBones, std::vector<float> &outFrameDeltas);
     void createDeltasSSBO();
-    void getBonePosition();
 };
 
 #endif // SMEARMESH_H_
